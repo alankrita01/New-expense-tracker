ecaea14b1669a83d670feabfe7cb7551cabb81cf
Alankrita Patidar <alankritapatidar@gmail.com>
2023-08-17 21:53:33 +0530

    updating premium feature file

--- controller/premiumFeature.js
+++ controller/premiumFeature.js
@@ -5,7 +5,7 @@ const Expense = require('../models/expenseModel');
 const sequelize = require('../util/database');
 
 const getUserLeaderBoard = async (req,res,next) => {
-    /*try{
+    try{
         const leaderboardOfUsers = await User.findAll({
             attributes: ['id', 'name', [sequelize.fn('sum', sequelize.col('expenses.amount')), 'total_cost']],
             include: [
@@ -18,30 +18,8 @@ const getUserLeaderBoard = async (req,res,next) => {
             order:[['total_cost','DESC']]
         })
         res.status(200).json(leaderboardOfUsers)
-    }*/
-    try{
-        const users = User.findAll();
-        const expenses = Expense.findAll();
-        const userAggregatedExpense = {}
-        console.log(expenses)
-
-        expenses.forEach((expense) => {
-
-            if(userAggregatedExpense[expense.userId]){
-                userAggregatedExpense[expense.userId] = userAggregatedExpense[expense.userId]+expense.amount;
-            }
-            else{
-                userAggregatedExpense[expense.userId]= expense.amount;
-            }
-        })
-
-        var userLeaderBoardDetails = [];
-        users.forEach((user) => {
-            userLeaderBoardDetails.push({name: user.name, total_cost: userAggregatedExpense[user.id]})
-        })
-        console.log(userLeaderBoardDetails);
-        res.status(200).json(userLeaderBoardDetails)
     }
+    
     catch(err){
         console.log(err)
         res.status(500).json(err)
