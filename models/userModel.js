6480a9bc34c1eb59765d0999e2d9a4f1da715497
Alankrita Patidar <alankritapatidar@gmail.com>
2023-08-29 21:22:28 +0530

    updating userModel.js file

--- models/userModel.js
+++ models/userModel.js
@@ -23,7 +23,7 @@ const User = sequelize.define('user',{
     ispremiumuser: Sequelize.BOOLEAN,
     totalExpenses: {
         type: Sequelize.INTEGER,
-        defaultValue: 0
+        defaultValue: 0,
     }
 })
 
